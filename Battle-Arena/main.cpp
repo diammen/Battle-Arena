855e8ce773c9c8d5d52ddbf4558361688acc6f59
Marcel Dao <marcelnamdao@gmail.com>
2018-09-19 17:52:06 -0700

    Battle Arena

--- Battle-Arena/main.cpp
+++ Battle-Arena/main.cpp
@@ -10,10 +10,18 @@ using std::cin;
 using namespace std::chrono_literals;
 using namespace std::this_thread;
 
-Character Hero = { 100, 10, 30, "Hero", "Attack" };
+Character Hero = { 100, 10, 30, "Hero", "Fimbul Lance" };
+Character Heroine = { 100, 10, 30, "Heroine", "Moldrak Thunder" };
 
 int main()
 {
+	bool gameOver = false;
 
-
+	while (!gameOver)
+	{
+		if (Hero.health == 0 & Heroine.health == 0)
+		{
+			gameOver = true;
+		}
+	}
 }
